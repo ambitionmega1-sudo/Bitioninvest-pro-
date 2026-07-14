b44801b33d9e82f3ba98c47eabbc5af4364fb641
ambitionmega1-sudo <ambitionmega1@gmail.com>
2026-07-14 23:23:16 +0100

    Connect dashboard to Firebase user data

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,56 @@
 
+import { auth, db } from "../firebase.js";
+
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+
+
+const userEmail = document.getElementById("userEmail");
+
+
+
+onAuthStateChanged(auth, async (user)=>{
+
+
+    if(user){
+
+
+        const userRef = doc(db,"users",user.uid);
+
+        const userSnap = await getDoc(userRef);
+
+
+
+        if(userSnap.exists()){
+
+
+            const data = userSnap.data();
+
+
+            userEmail.innerHTML =
+            "Welcome, " + data.name +
+            "<br>Email: " + data.email;
+
+
+        }
+
+
+    } else {
+
+
+        window.location.href="login.html";
+
+
+    }
+
+
+});
