6d4e1b8687951e0e06fbf73a56ba0d0330385a21
ambitionmega1-sudo <ambitionmega1@gmail.com>
2026-07-14 23:19:22 +0100

    Add firebase login system

--- js/login.js
+++ js/login.js
@@ -1,1 +1,67 @@
 
+import { auth } from "../firebase.js";
+
+
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+
+
+const loginForm = document.getElementById("loginForm");
+
+
+
+if(loginForm){
+
+
+loginForm.addEventListener("submit", async (e)=>{
+
+
+    e.preventDefault();
+
+
+    const email = document.getElementById("loginEmail").value;
+
+    const password = document.getElementById("loginPassword").value;
+
+    const message = document.getElementById("loginMessage");
+
+
+
+    try{
+
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+
+        message.innerHTML =
+        "Login successful!";
+
+
+        setTimeout(()=>{
+
+            window.location.href="dashboard.html";
+
+        },1500);
+
+
+
+    }catch(error){
+
+
+        message.innerHTML =
+        error.message;
+
+
+    }
+
+
+});
+
+
+}
