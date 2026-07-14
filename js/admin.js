1db0a07084633da459b0e4526ff7af707c30a1e6
ambitionmega1-sudo <ambitionmega1@gmail.com>
2026-07-14 23:34:22 +0100

    Add admin balance management system

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,127 @@
 
+import { db } from "../firebase.js";
+
+
+import {
+    collection,
+    getDocs,
+    doc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+
+
+const userList = document.getElementById("userList");
+
+const updateBtn = document.getElementById("updateBalance");
+
+const message = document.getElementById("adminMessage");
+
+
+
+async function loadUsers(){
+
+
+    const usersSnapshot = await getDocs(
+        collection(db,"users")
+    );
+
+
+    userList.innerHTML = "";
+
+
+    usersSnapshot.forEach((user)=>{
+
+
+        const data = user.data();
+
+
+        userList.innerHTML += `
+
+        <div class="card">
+
+        <p>
+        <b>Name:</b> ${data.name}
+        </p>
+
+        <p>
+        <b>Email:</b> ${data.email}
+        </p>
+
+        <p>
+        <b>Balance:</b> $${data.balance}
+        </p>
+
+        <p>
+        <b>ID:</b> ${user.id}
+        </p>
+
+        </div>
+
+        `;
+
+
+    });
+
+
+}
+
+
+
+updateBtn.addEventListener("click", async ()=>{
+
+
+    const id =
+    document.getElementById("userId").value;
+
+
+    const balance =
+    document.getElementById("newBalance").value;
+
+
+
+    if(!id || !balance){
+
+        message.innerHTML =
+        "Please enter user ID and balance";
+
+        return;
+
+    }
+
+
+
+    try{
+
+
+        await updateDoc(
+            doc(db,"users",id),
+            {
+                balance:Number(balance)
+            }
+        );
+
+
+        message.innerHTML =
+        "Balance updated successfully";
+
+
+        loadUsers();
+
+
+
+    }catch(error){
+
+
+        message.innerHTML =
+        error.message;
+
+
+    }
+
+
+});
+
+
+
+loadUsers();
